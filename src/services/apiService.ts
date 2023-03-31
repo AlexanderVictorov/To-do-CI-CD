56954a0ae2fa370d8cc29082c8041ad308fec18b
AnyMac <anymacstore@MacBook-Pro.local>
2023-03-31 10:18:25 +0300

    feat: install json db server, created apiService

---
+++ src/services/apiService.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+import { BASE_URL } from 'config';
+
+// Create a new instance of Axios
+const $api:AxiosInstance = axios.create({
+  withCredentials: true,
+  baseURL: BASE_URL,
+});
+
+$api.interceptors.response.use(
+  (response: AxiosResponse) => {
+    // Do something with the response data
+    console.log('Response was received:', response);
+
+    // You must return the response object in order to continue the response
+    return response;
+  },
+  (error) => {
+    // Do something with the response error
+    console.error('Error in response interceptor:', error);
+    return Promise.reject(error);
+  }
+);
